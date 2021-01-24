d4848243b7a924632311b0113defbbc305c15db7
Daniela Valladares <danivvmtz_56@hotmail.com>
2021-01-24 00:00:22 -0600

    short-term test cases

--- webdriverio-test/test/specs/basic.js
+++ webdriverio-test/test/specs/basic.js
@@ -65,6 +65,73 @@ describe('Parking page', () => {
         expect(result).toHaveTextContaining("$ 2.00");
     });
     
+    it('Short-Term: should stay for two hours', () => {
+        ParkingLot = $("#ParkingLot > option:nth-child(2)");
+        ParkingLot.click();
+        result= $("body > form > table > tbody > tr:nth-child(4) > td:nth-child(2) > span.SubHead > b");
+        Calculatebutton = $("body > form > input[type=submit]:nth-child(3)");
+        inputStart = $('#StartingDate');
+        inputFinish = $('#LeavingDate');
+        inputStartTime = $('#StartingTime');
+        inputEndTime = $('#LeavingTime');
+        inputStart.setValue('1/21/2021');
+        inputFinish.setValue('1/21/2021');
+        inputStartTime.setValue('12:00');
+        inputEndTime.setValue('2:00');
+        Calculatebutton.click();
+        expect(result).toHaveTextContaining("$ 4.00");
+    });
+
+    it('Short-Term: should stay for five hours and a half', () => {
+        ParkingLot = $("#ParkingLot > option:nth-child(2)");
+        ParkingLot.click();
+        result= $("body > form > table > tbody > tr:nth-child(4) > td:nth-child(2) > span.SubHead > b");
+        Calculatebutton = $("body > form > input[type=submit]:nth-child(3)");
+        inputStart = $('#StartingDate');
+        inputFinish = $('#LeavingDate');
+        inputStartTime = $('#StartingTime');
+        inputEndTime = $('#LeavingTime');
+        inputStart.setValue('1/21/2021');
+        inputFinish.setValue('1/21/2021');
+        inputStartTime.setValue('12:00');
+        inputEndTime.setValue('5:30');
+        Calculatebutton.click();
+        expect(result).toHaveTextContaining("$ 11.00");
+    });
+
+    it('Short-Term: should stay for a complete day', () => {
+        ParkingLot = $("#ParkingLot > option:nth-child(2)");
+        ParkingLot.click();
+        result= $("body > form > table > tbody > tr:nth-child(4) > td:nth-child(2) > span.SubHead > b");
+        Calculatebutton = $("body > form > input[type=submit]:nth-child(3)");
+        inputStart = $('#StartingDate');
+        inputFinish = $('#LeavingDate');
+        inputStartTime = $('#StartingTime');
+        inputEndTime = $('#LeavingTime');
+        inputStart.setValue('1/21/2021');
+        inputFinish.setValue('1/22/2021');
+        inputStartTime.setValue('12:00');
+        inputEndTime.setValue('12:00');
+        Calculatebutton.click();
+        expect(result).toHaveTextContaining("$ 24.00");
+    });
+
+    it('Short-Term: should stay for a day and 2 hours', () => {
+        ParkingLot = $("#ParkingLot > option:nth-child(2)");
+        ParkingLot.click();
+        result= $("body > form > table > tbody > tr:nth-child(4) > td:nth-child(2) > span.SubHead > b");
+        Calculatebutton = $("body > form > input[type=submit]:nth-child(3)");
+        inputStart = $('#StartingDate');
+        inputFinish = $('#LeavingDate');
+        inputStartTime = $('#StartingTime');
+        inputEndTime = $('#LeavingTime');
+        inputStart.setValue('1/21/2021');
+        inputFinish.setValue('1/22/2021');
+        inputStartTime.setValue('12:00');
+        inputEndTime.setValue('2:00');
+        Calculatebutton.click();
+        expect(result).toHaveTextContaining("$ 28.00");
+    });
 
 })
 
