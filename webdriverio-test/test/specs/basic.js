1d6bf7c74f3ffe575fd8a61a2ba6af67b523f11d
Daniela Valladares <danivvmtz_56@hotmail.com>
2021-01-23 23:11:16 -0600

    one day

--- webdriverio-test/test/specs/basic.js
+++ webdriverio-test/test/specs/basic.js
@@ -3,11 +3,21 @@ describe('Parking page', () => {
         browser.url('http://www.shino.de/parkcalc/')
         expect(browser).toHaveTitle('Parking Cost Calculator');
     })
+
+    it('should stay for a day', () => {
+        const result= "/html/body/form/table/tbody/tr[4]/td[2]/span[1]/b";
+        const Calculatebutton = "/html/body/form/input[2]";
+        inputStart = $('#StartingDate');
+        inputFinish = $('#LeavingDate');
+        inputStart.setValue('1/21/2021');
+        inputFinish.setValue('1/22/2021');
+        Calculatebutton.click();
+        expect(result).toHaveTextContaining("$18.00");
+    });
+
+
+    
 })
 
 
-it('should demonstrate the getValue command', () => {
-    const inputUser = $('#StartingDate');
-    input.addValue('21/01/21')
-    console.log(value); // outputs: "John Doe"
-});
+
