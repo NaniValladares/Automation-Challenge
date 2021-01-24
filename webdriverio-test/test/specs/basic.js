a10b02f3b3b2d0e4688bf7b17b7c2732c845eb82
Daniela Valladares <danivvmtz_56@hotmail.com>
2021-01-23 23:28:34 -0600

    valet parking test cases

--- webdriverio-test/test/specs/basic.js
+++ webdriverio-test/test/specs/basic.js
@@ -4,20 +4,33 @@ describe('Parking page', () => {
         expect(browser).toHaveTitle('Parking Cost Calculator');
     })
 
-    it('should stay for a day', () => {
-        const result= "/html/body/form/table/tbody/tr[4]/td[2]/span[1]/b";
-        const Calculatebutton = "/html/body/form/input[2]";
+    // Valet Parking Test Cases
+    it('Valet Parking: should stay for a day', () => {
+        result= $("body > form > table > tbody > tr:nth-child(4) > td:nth-child(2) > span.SubHead > b");
+        Calculatebutton = $("body > form > input[type=submit]:nth-child(3)");
         inputStart = $('#StartingDate');
         inputFinish = $('#LeavingDate');
         inputStart.setValue('1/21/2021');
         inputFinish.setValue('1/22/2021');
         Calculatebutton.click();
-        expect(result).toHaveTextContaining("$18.00");
+        expect(result).toHaveTextContaining("$ 18.00");
     });
 
+    it('Valet Parking: should stay for five hours or less', () => {
+        result= $("body > form > table > tbody > tr:nth-child(4) > td:nth-child(2) > span.SubHead > b");
+        Calculatebutton = $("body > form > input[type=submit]:nth-child(3)");
+        inputStartDate = $('#StartingDate');
+        inputEndDate = $('#LeavingDate');
+        inputStartTime = $('#StartingTime');
+        inputEndTime = $('#LeavingTime');
+        inputStartDate.setValue('1/21/2021');
+        inputEndDate.setValue('1/21/2021');
+        inputStartTime.setValue('12:00');
+        inputEndTime.setValue('3:00');
+        Calculatebutton.click();
+        expect(result).toHaveTextContaining("$ 12.00");
+    });
 
     
 })
-
-
 
